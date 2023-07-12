b40a7c73bde67b1936750c72b5a5ffebad0f3115
meesdekker.xyz <me@meesdekker.xyz>
2023-07-12 22:01:01 +0200

    🐛 fix(ft_realloc.c): change return type of ft_realloc function from void to void* 🔒 chore(ft_realloc.c): add error handling for ft_calloc failure

--- memory/ft_realloc.c
+++ memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/12 21:52:28 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/12 21:57:14 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/12 22:00:46 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@
  * @return char* The reallocated string
  */
 
-void	ft_realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t size)
 {
 	void	*new;
 
 	new = ft_calloc(1, size);
 	if (!new)
-		return ;
+		return (NULL);
 	ft_memcpy(new, ptr, size);
 	free(ptr);
 	return (new);
