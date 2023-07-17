7e3359bb0f06555276c0f73527883b8dd8ea957d
meesdekker.xyz <me@meesdekker.xyz>
2023-07-17 23:17:17 +0200

    🐛 fix(ft_vec_free.c): fix off-by-one error in the loop condition 🔥 chore(ft_vec_free.c): remove unnecessary code for freeing vector data

--- memory/vector/ft_vec_free.c
+++ memory/vector/ft_vec_free.c
@@ -6,7 +6,7 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/15 22:34:11 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/17 22:19:21 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/17 23:17:01 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,11 @@ void	ft_vec_free(t_vector *vec, void (*free_data)(void *))
 {
 	size_t	i;
 
-	i = 0;
+	i = -1;
 	if (free_data)
-	{
-		while (i < vec->size)
-		{
+		while (i++ < vec->size)
 			free_data(vec->data + i * vec->type_size);
-			i++;
-		}
-	}
-	if (vec->data != NULL)
-	{
-		free(vec->data);
-		vec->data = NULL;
-	}
+	vec->data = NULL;
 	vec->size = 0;
 	vec->type_size = 0;
 }
