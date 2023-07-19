db80f60ac7b1913e4fddb28b5edf3fe6cd9f3fe7
meesdekker.xyz <me@meesdekker.xyz>
2023-07-19 21:33:07 +0200

    Co-authored-by: JdeBaaij <JdeBaaij@users.noreply.github.com>

--- memory/vector/ft_vec_free.c
+++ memory/vector/ft_vec_free.c
@@ -6,7 +6,7 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/15 22:34:11 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/18 21:09:25 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/19 21:31:08 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
  * @param vec The vector to free
  * @param f The function to free the data in the vector
  */
-void	ft_vec_free(t_vector *vec, void (*f)(void *))
+void ft_vec_free(t_vector *vec, void (*f)(void *))
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	if (f)
@@ -31,6 +31,7 @@ void	ft_vec_free(t_vector *vec, void (*f)(void *))
 			i++;
 		}
 	}
+	free(vec->data);
 	vec->data = NULL;
 	vec->lenght = 0;
 	vec->capacity = 0;
