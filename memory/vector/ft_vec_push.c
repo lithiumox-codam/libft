cbff6b6c04ac529b89480b00d43b0d4533f0ac6c
meesdekker.xyz <me@meesdekker.xyz>
2023-07-20 14:08:31 +0200

    🔧 chore(ft_vec_free.c): refactor ft_vec_free function 🔧 chore(ft_vec_push.c): refactor ft_vec_push function

--- memory/vector/ft_vec_push.c
+++ memory/vector/ft_vec_push.c
@@ -6,7 +6,7 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/15 22:26:05 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/19 22:05:43 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/20 14:08:08 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@
  * @param data The data to push
  * @return bool true when succeeded, false when failed
  */
-bool ft_vec_push(t_vector *vec, void *data)
+bool	ft_vec_push(t_vector *vec, void *data)
 {
-	void *new;
+	void	*new;
 
 	if (vec->lenght == vec->capacity)
 	{
