b8047815b1484156db39153b5e2c77e62427cf2a
meesdekker.xyz <me@meesdekker.xyz>
2023-07-21 17:27:25 +0200

    🐛 fix(ft_vec_get.c): fix index check condition in ft_vec_get function

--- memory/vector/ft_vec_get.c
+++ memory/vector/ft_vec_get.c
@@ -6,7 +6,7 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/15 22:33:02 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/21 12:59:18 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/21 17:19:59 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@
  */
 void	*ft_vec_get(t_vector *vec, size_t index)
 {
-	if (vec == NULL)
-		return (NULL);
-	if (index <= 0)
+	if (index < 0)
 		return (NULL);
 	if (index >= vec->lenght)
 		return (NULL);
