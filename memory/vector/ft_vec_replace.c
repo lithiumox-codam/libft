cc5f2d5fd37222137f8c92e39f47c881cf8f9ea0
meesdekker.xyz <me@meesdekker.xyz>
2023-07-24 10:37:25 +0200

    🐛 fix(ft_vec_replace.c): simplify ft_vec_replace function and remove unnecessary code

--- memory/vector/ft_vec_replace.c
+++ memory/vector/ft_vec_replace.c
@@ -6,52 +6,27 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/21 13:09:13 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/24 09:46:24 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/24 10:36:31 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
 /**
- * @brief Replaces one element in the vector and frees it then proceeds to
- * replace it with one or more elements provided by data
+ * @brief Replaces one element in the vector with the provided data
  *
  * @param vec The vector to replace in
  * @param index The index of the element to replace
- * @param data The data to replace with (can be multiple elements)
+ * @param data The data to replace with
  *
- * @return void* The pointer to the first element of the replaced data
- * or NULL if the replacement failed
- *
- * @note The data must be NULL terminated and the vector must be able to
- * contain the data that is being inserted
- */
-void	*ft_vec_replace(t_vector *vec, size_t index, void **data)
-{
-	size_t	data_count;
-	size_t	i;
 
-	data_count = 0;
-	if (vec->f)
-		vec->f(vec->data + index * vec->type_size);
-	while (data[data_count])
-		data_count++;
-	if (data_count == 0)
+	* @return void* The pointer to the replaced element or NULL if the replacement failed
+ *
+ * @note The vector must be able to contain the data that is being inserted
+ */
+void	*ft_vec_replace(t_vector *vec, size_t index, void *data)
+{
+	if (!ft_vec_set(vec, index, data))
 		return (NULL);
-	if (data_count == 1)
-	{
-		if (!ft_vec_set(vec, index, data[0]))
-			return (NULL);
-		return (vec->data + index * vec->type_size);
-	}
-	data_count--;
-	i = 0;
-	while (!data[i] && i < data_count)
-	{
-		if (!ft_vec_insert(vec, index + i, data[i]))
-			return (NULL);
-		i++;
-	}
-	vec->lenght += data_count;
 	return (vec->data + index * vec->type_size);
 }
