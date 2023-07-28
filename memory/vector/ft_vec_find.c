f37388cc43e7b2aef7455091ce60648eb84d71bf
meesdekker.xyz <me@meesdekker.xyz>
2023-07-28 17:44:55 +0200

    🐛 fix(ft_vec_find.c): fix memory allocation bug in ft_vec_find function ✨ feat(ft_vec_find.c): add emergency_free function to free memory in case of error ✨ feat(ft_vec_find.c): add find_matches function to find all occurrences of a comparison in the vector

--- memory/vector/ft_vec_find.c
+++ memory/vector/ft_vec_find.c
@@ -6,7 +6,7 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/21 02:41:44 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/28 15:36:06 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/28 17:44:44 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,38 @@ static t_found	*create_struct(void *data, size_t index)
 	return (found);
 }
 
+static void	emergency_free(t_found **found, size_t count)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(found[i]);
+		i++;
+	}
+	free(found);
+}
+
+static bool	find_matches(t_vector *vec, bool (*cmp)(void *), t_found **found)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < vec->lenght)
+	{
+		if (cmp(vec->data + i * vec->type_size) == true)
+		{
+			found[j] = create_struct(vec->data + i * vec->type_size, i);
+			if (!found[j])
+				return (emergency_free(found, j), false);
+			j++;
+		}
+		i++;
+	}
+}
+
 /**
  * @brief Finds the all occurences of a comparison in the vector
  *
@@ -39,24 +71,14 @@ t_found	**ft_vec_find(t_vector *vec, bool (*cmp)(void *))
 	t_found	**found;
 
 	i = 0;
-	count = 0;
-	found = malloc(sizeof(t_found) * (ft_vec_count(vec, cmp) + 1));
+	count = ft_vec_count(vec, cmp);
+	if (count == 0)
+		return (NULL);
+	found = malloc(sizeof(t_found *) * (count + 1));
 	if (!found)
 		return (NULL);
-	while (i < vec->lenght)
-	{
-		if (cmp(vec->data + i * vec->type_size) == true)
-		{
-			found[count] = create_struct(vec->data + i * vec->type_size, i);
-			if (!found[count])
-				return (NULL);
-			count++;
-		}
-		i++;
-	}
-	if (count == 0)
-		free(found);
-	else
-		found[count] = NULL;
+	count = 0;
+	find_matches(vec, cmp, found);
+	found[count] = NULL;
 	return (found);
 }
