0e5e1e9098660fc8cba4a37593d8a5c3cd3c6efe
meesdekker.xyz <me@meesdekker.xyz>
2023-07-28 15:37:33 +0200

    🐛 fix(ft_vec_find.c): fix count variable initialization and assignment in ft_vec_find function

--- memory/vector/ft_vec_find.c
+++ memory/vector/ft_vec_find.c
@@ -6,7 +6,7 @@
 /*   By: mdekker <mdekker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/21 02:41:44 by mdekker       #+#    #+#                 */
-/*   Updated: 2023/07/24 09:47:05 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/07/28 15:36:06 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,9 @@ t_found	**ft_vec_find(t_vector *vec, bool (*cmp)(void *))
 	t_found	**found;
 
 	i = 0;
-	count = ft_vec_count(vec, cmp);
-	found = malloc(sizeof(t_found) * (count + 1));
-	if (!found || count == 0)
+	count = 0;
+	found = malloc(sizeof(t_found) * (ft_vec_count(vec, cmp) + 1));
+	if (!found)
 		return (NULL);
 	while (i < vec->lenght)
 	{
@@ -50,10 +50,13 @@ t_found	**ft_vec_find(t_vector *vec, bool (*cmp)(void *))
 			found[count] = create_struct(vec->data + i * vec->type_size, i);
 			if (!found[count])
 				return (NULL);
-			count--;
+			count++;
 		}
 		i++;
 	}
-	found[count] = NULL;
+	if (count == 0)
+		free(found);
+	else
+		found[count] = NULL;
 	return (found);
 }
